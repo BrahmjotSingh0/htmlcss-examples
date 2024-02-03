aee81f79221d03e318858bc4935d6786da083c33
brahmjot singh <brahmjots111@gmail.com>
2024-02-03 22:39:34 +0530

    add history button for calculator

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,3 +1,5 @@
+var calculationHistory = [];
+
 function clrscr() {
     document.getElementById("result").value = "";
 }
@@ -10,4 +12,9 @@ function calc() {
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
+    calculationHistory.push(p + " = " + q);
+}
+
+function showHistory() {
+    alert("Calculation History:\n" + calculationHistory.join("\n"));
 }
